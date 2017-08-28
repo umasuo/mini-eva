47b713f3d11187bea459fb9c5725b054900a06cf
umasuo <liuquan89@gmail.com>
2017-08-28 10:40:56 +0800

    update

--- src/main/java/cn/eva/mini/infra/auth/AuthPolicyProvider.java
+++ src/main/java/cn/eva/mini/infra/auth/AuthPolicyProvider.java
@@ -15,47 +15,10 @@ public interface AuthPolicyProvider {
   /**
    * check the authentication of the token
    *
-   * @param subjectId subject Id, this can be: customer id, service id, anonymous id.
    * @param token     token input.
    */
-  default void checkToken(String subjectId, Token token) {
-    Assert.notNull(subjectId);
-    Assert.notNull(token);
-
-    // check if the token belong to the subject.
-    checkSubjectId(subjectId, token.getSubjectId());
-    //check expire time
-    checkExpireTime(token.getGenerateTime(), token.getExpiresIn());
-    // check if the token has the correct policy.
-    checkScope(token.getScopes());
-    //black list for disable a token.
-    checkBlackList(token.getTokenId());
+  default void checkToken(String token) {
+    //todo
   }
 
-
-  default void checkSubjectId(String idInput, String idInToken) {
-    if (!StringUtils.equals(idInput, idInToken)) {
-      throw new AuthFailedException("Token is illegal：token not belong to " + idInput);
-    }
-  }
-
-  /**
-   * check if the token has expired.
-   *
-   * @param generateTime token's generate time.
-   * @param expireIn     duration that token can be legal.
-   */
-  default void checkExpireTime(long generateTime, long expireIn) {
-
-    //here we should consider about different time zone
-    long curTime = System.currentTimeMillis();
-    long expectedExpireTime = generateTime + expireIn;
-    if (expectedExpireTime < curTime) {
-      throw new AuthFailedException("Token is illegal： token has expired.");
-    }
-  }
-
-  void checkScope(List<Scope> scopes);
-
-  void checkBlackList(String tokenId);
 }
